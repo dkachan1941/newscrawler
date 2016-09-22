c0562edd10e51000be422dd4f9c0884a35af280c
Качан Дмитрий <kachan_dl@magnit.ru>
2016-09-22 16:50:24 +0300

    first

---
+++ newscrawler/crawler/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from django.utils import timezone
+from django.db import models
+
+class News(models.Model):
+	title = models.CharField(max_length=250)
+	text = models.TextField()
+	date = models.DateTimeField()
+	category = models.CharField(max_length=250)
+
+	def __str__(self):
+		return '%s' % (self.title)
+
+	class Meta:
+		verbose_name = "Новость"
+		verbose_name_plural = "Новости"
+
+	def __unicode__(self):
+		return u"%s" % (self.title)
